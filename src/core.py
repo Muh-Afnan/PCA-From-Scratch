2d8b1db27bed2f11f1ff622260502fab0700b01c
Muh-Afnan <muhammadafnan108@gmail.com>
2026-04-15 18:36:01 +0500

    Refactor covariance_matrix function and remove unused mean calculation

--- src/core.py
+++ src/core.py
@@ -1,38 +1,3 @@
-def calculate_mean(data):
-    """Calculate the mean of a list of numbers.
-
-    Args:
-        data (list): A list of lists of numbers.
-    Returns:
-        list: A list of means corresponding to each inner list.
-    """
-    data = data[[list(row) for row in zip(*data)]]
-    mean_list = []
-    for row in data:
-        mean = sum(row) / len(row)
-        mean_list.append(mean)
-
-    return mean_list
-
-def remove_mean(data):
-    """Remove the mean from each element in a list of lists of numbers.
-
-    Args:
-        data (list): A list of lists of numbers.
-    Returns:
-        list: A list of lists with the mean removed from each element.
-    """
-    data = data[[list(row) for row in zip(*data)]]
-    mean_list = calculate_mean(data)
-    new_data = []
-    for i in range(len(mean_list)):
-        normalized_data = [data[i][j] - mean_list[i] for j in range(len(data[i]))]
-        new_data.append(normalized_data)
-    
-    new_data = [list(row) for row in zip(*new_data)]
-
-    return new_data
-
 def covariance_matrix(data):
     """Calculate the covariance matrix of a list of lists of numbers.
 
@@ -41,9 +6,49 @@ def covariance_matrix(data):
     Returns:
         list: A covariance matrix as a list of lists.
     """
-    normalized_data = remove_mean(data)
-    normalized_trans_data = normalized_data[[list(row) for row in zip(*normalized_data)]]
-    feature_sum = [sum(row) for row in normalized_trans_data]
-    for i in range(len(feature_sum)):
-        for j in range
-        feature_sum[i] = feature_sum[i] / (len(normalized_data) - 1)
+
+    features = list(zip(*data))
+
+    center = []
+    for feature in features:
+        mean = sum(feature) / len(feature)
+        center.append([x - mean for x in feature])
+
+    cov_matrix = []
+    for i in range(len(center)):
+        row = []
+        for j in range(len(center)):
+            single_cov = sum(
+                center[i][k] * center[j][k] for k in range(len(center[i]))
+            ) / (len(center[i]) - 1)
+            row.append(single_cov)
+        cov_matrix.append(row)
+    return cov_matrix
+
+
+def corelation_matrix(data):
+    """Calculate the correlation matrix of a list of lists of numbers.
+
+    Args:
+        data (list): A list of lists of numbers.
+    Returns:
+        list: A correlation matrix as a list of lists.
+    """
+
+    cov_matrix = covariance_matrix(data)
+
+
+data = [
+    [2.5, 2.4, 1.7],
+    [0.5, 0.7, 1.9],
+    [2.2, 2.9, 2.1],
+    [1.9, 2.2, 1.8],
+    [3.1, 3.0, 2.3],
+    [2.3, 2.7, 2.0],
+    [2, 1.6, 1.5],
+    [1, 1.1, 8],
+    [1.5, 1.6, 8],
+    [1.1, 0.9, 9],
+]
+cov_matrix = covariance_matrix(data)
+print(cov_matrix)
